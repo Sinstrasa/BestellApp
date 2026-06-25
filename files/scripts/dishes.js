e1b7c3e3421df1908cd4f1076edd41844b170aac
Erik S. <eriksom764@gmail.com>
2026-06-25 15:33:06 +0200

    added content for dishes

---
+++ files/scripts/dishes.js
@@ -0,0 +1,96 @@
+let dishes = [
+  {
+    Burger: [
+      {
+        name: "Veggie mushroom black burger",
+        Ingrdients: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        price: 16.9,
+        picture: "/assets/images/burger/veggie.png",
+        amount: 0,
+      },
+      {
+        name: "All meat burger",
+        Ingrdients:
+          "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ Sauce",
+        price: 15.9,
+        picture: "/assets/images/burger/all-meat.png",
+        amount: 0,
+      },
+      {
+        name: "Beef ref burger",
+        Ingrdients: "Beeg, Cheese, Tomatoes, Lettuce, Onion",
+        price: 14.9,
+        picture: "/assets/images/burger/beef-red.png",
+        amount: 0,
+      },
+      {
+        name: "Big Chicken burger",
+        Ingrdients: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        price: 15.9,
+        picture: "/assets/images/burger/big-chicken.png",
+        amount: 0,
+      },
+    ],
+    Pizza: [
+      {
+        name: "Pizza Margherita",
+        Ingrdients: "Tomato Sauce, Mozzarella",
+        price: 11.9,
+        picture: "/assets/images/pizza/margherita.png",
+        amount: 0,
+      },
+      {
+        name: "Pizza Chorizo",
+        Ingrdients: "Tomato slices, Mozzarella, Chorizo",
+        price: 13.9,
+        picture: "/assets/images/pizza/chorizo.png",
+        amount: 0,
+      },
+      {
+        name: "Pizza Funghi",
+        Ingrdients: "Red onion, Olives, Button Mushrooms, Mozzarella",
+        price: 12.9,
+        picture: "/assets/images/pizza/funghi.png",
+        amount: 0,
+      },
+      {
+        name: "Quattro Formaggi with Chicken",
+        Ingrdients:
+          "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+        price: 15.9,
+        picture: "/assets/images/pizza/formaggi.png",
+        amount: 0,
+      }
+    ],
+    Salad: [
+      {
+        name: "Warm beef arugula salad",
+        Ingrdients: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamico-vinegar dressing",
+        price: 16.9,
+        picture: "/assets/images/pizza/warm-beef.png",
+        amount: 0,
+      },
+      {
+        name: "Mini green Salad",
+        Ingrdients: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+        price: 7.9,
+        picture: "/assets/images/pizza/mini-green.png",
+        amount: 0,
+      },
+      {
+        name: "Green Salad with sea food",
+        Ingrdients: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimps, Dijon mustard-lemon dressing with dill",
+        price: 16.9,
+        picture: "/assets/images/pizza/seafood.png",
+        amount: 0,
+      },
+      {
+        name: "Vegan green salad with tofu",
+        Ingrdients: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Redishes, Bittercress, Tofu, Peanuts",
+        price: 14.9,
+        picture: "/assets/images/pizza/vegan.png",
+        amount: 0,
+      }
+    ],
+  },
+];
